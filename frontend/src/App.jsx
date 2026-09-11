cb03283cbbe2b521e289fccbed55e110275b4058
Jeferson004 <jeffmuring12@gmail.com>
2026-09-11 18:36:56 +0800

    fix: minor fix in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductForm from './components/ProductForm/ProductForm.jsx';
 import ProductTable from './components/ProductTable/ProductTable.jsx';
 import Home from './pages/Home/Home.jsx';
@@ -28,19 +28,26 @@ function App() {
   };
 
   return (
-    <main>
-      <Home />
-
-      {showForm && (
-        <ProductForm
-          selectedProduct={selectedProduct}
-          onSuccess={handleSuccess}
-          onCancel={handleCloseForm}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/products"
+          element={
+            <>
+              {showForm && (
+                <ProductForm
+                  selectedProduct={selectedProduct}
+                  onSuccess={handleSuccess}
+                  onCancel={handleCloseForm}
+                />
+              )}
+              <ProductTable onAdd={handleAdd} onEdit={handleEdit} />
+            </>
+          }
         />
-      )}
-
-      <ProductTable onAdd={handleAdd} onEdit={handleEdit} />
-    </main>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
